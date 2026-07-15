955cd08d796df975250838e1232a344019395982
RSeguraHexe <rsegurah@unjbg.edu.pe>
2026-07-14 21:21:42 -0500

    Creacion

---
+++ proyecto/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string>
+
+int main(){
+	std::cout<<"===================================\n";
+	std::cout<<" Eliga una opcion!\n";
+	std::cout<<"===================================\n";
+}
